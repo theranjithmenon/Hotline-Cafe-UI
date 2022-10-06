151f05224db61a23b2c3769d87e1fe4a875cc447
Ranjith <ranjithmenon112@gmail.com>
2022-10-06 21:30:22 +0530

    Hotline Cafe

--- lib/Pages/Home/Discovery.dart
+++ lib/Pages/Home/Discovery.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
 
-
 class Discovery extends StatelessWidget {
   const Discovery({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+      length: 2,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Row(children: [
+              const Text(
+                  "Explore",
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
+                ),
+                const Spacer(),
+                IconButton(
+                    onPressed: () {},
+                    icon: Image.asset("assets/images/Home/Chart.png")),
+            ],),
+          )
+        ],
+      ),
+    );
   }
 }
