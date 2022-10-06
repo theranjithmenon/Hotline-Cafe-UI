b0f84d5d0eeee273d5a3c2c17a635d1daf404894
Ranjith <ranjithmenon112@gmail.com>
2022-10-06 21:30:38 +0530

    Hotline Cafe

--- lib/Pages/Home/Discovery.dart
+++ lib/Pages/Home/Discovery.dart
@@ -6,13 +6,14 @@ class Discovery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 2,
+      length: 6,
       child: Column(
         children: [
           Padding(
             padding: const EdgeInsets.all(10),
-            child: Row(children: [
-              const Text(
+            child: Row(
+              children: [
+                const Text(
                   "Explore",
                   style: TextStyle(fontSize: 28, fontWeight: FontWeight.w700),
                 ),
@@ -20,10 +21,68 @@ class Discovery extends StatelessWidget {
                 IconButton(
                     onPressed: () {},
                     icon: Image.asset("assets/images/Home/Chart.png")),
-            ],),
-          )
+              ],
+            ),
+          ),
+          tabbar()
         ],
       ),
+    );
+  }
+
+  tabbar() {
+    return const SizedBox(
+      height: 50,
+      child: TabBar(tabs: [
+        SizedBox(
+            height: 50,
+            child: Center(
+              child: Text(
+                "All",
+                style: TextStyle(fontSize: 10),
+              ),
+            )),
+        SizedBox(
+            height: 50,
+            child: Center(
+              child: Text(
+                "Popular",
+                style: TextStyle(fontSize: 10),
+              ),
+            )),
+        SizedBox(
+            height: 50,
+            child: Center(
+              child: Text(
+                "Live",
+                style: TextStyle(fontSize: 10),
+              ),
+            )),
+        SizedBox(
+            height: 50,
+            child: Center(
+              child: Text(
+                "VS",
+                style: TextStyle(fontSize: 10),
+              ),
+            )),
+        SizedBox(
+            height: 50,
+            child: Center(
+              child: Text(
+                "Nearby",
+                style: TextStyle(fontSize: 10),
+              ),
+            )),
+        SizedBox(
+            height: 50,
+            child: Center(
+              child: Text(
+                "New",
+                style: TextStyle(fontSize: 10),
+              ),
+            )),
+      ]),
     );
   }
 }
